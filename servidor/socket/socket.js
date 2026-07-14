9eb7ebcc3342649f7af2c1266f28acda3a327e26
Selkis90 <88568286+Selkis90@users.noreply.github.com>
2026-07-13 19:25:28 -0500

    Track connected clients in socket server
    mejora

--- servidor/socket/socket.js
+++ servidor/socket/socket.js
@@ -1,10 +1,18 @@
 module.exports = function(io){
 
+    // Lista de clientes conectados
+    const clientes = [];
+
     io.on("connection", (socket)=>{
+
+        // Guardar el nuevo cliente
+        clientes.push(socket.id);
 
         console.log("=================================");
         console.log("Nuevo cliente conectado");
         console.log("ID:", socket.id);
+        console.log("Clientes conectados:", clientes.length);
+        console.log("Lista:", clientes);
         console.log("=================================");
 
         socket.emit("mensaje",{
@@ -13,7 +21,15 @@ module.exports = function(io){
 
         socket.on("disconnect",()=>{
 
+            const indice = clientes.indexOf(socket.id);
+
+            if(indice !== -1){
+                clientes.splice(indice,1);
+            }
+
             console.log("Cliente desconectado:",socket.id);
+            console.log("Clientes conectados:", clientes.length);
+            console.log("Lista:", clientes);
 
         });
 
